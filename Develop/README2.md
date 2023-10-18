c3e3ce05e337069e8bbe1408c321f1a76d173f33
MAT-2 <mpcasey30@gmail.com>
2023-10-18 15:18:53 -0500

    Created the inquirer prompt method to take all the questions from the const questions variable. Then take those reponses in a function to use in a writeToFile. In its parameters, we wanted to create a README2.md file, take and use the generateMarkdown function and using the responses as the data. We also tested using console.log.

---
+++ Develop/README2.md
@@ -0,0 +1,27 @@
+# undefined
+
+  ## Table of Contents
+  1. [Description] (#description)
+  2. [Installation] (#installation)
+  3. [Usage] (#usage)
+  4. [License] (#license)
+  5. [Contributors] (#contributors)
+  6. [Testing Instructions] (#testing instructions)
+
+  ## Description
+  it is super fun
+
+  ## Installation
+  NPM
+
+  ## Usage
+  To learn;
+
+  ## License
+  Please refer to MIT License
+
+  ## Contributors
+  None
+
+  ## Testing Instructions
+  None
--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -49,12 +50,18 @@ const questions = [
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
-    err ? console.log(error) : console.log("Success!");
+    err ? console.log(err) : console.log("Success!");
   });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    writeToFile("README2.md", generateMarkdown(response));
+    response
+      ? console.log(generateMarkdown(response))
+      : console.log("Please input info to questions.");
+  });
+}
 // Function call to initialize app
 init();
